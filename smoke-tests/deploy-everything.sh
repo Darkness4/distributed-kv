2b4013c6a64759d693439f9771765412a9cafa1e
Nguyen Marc <nguyen_marc@live.fr>
2024-03-14 02:21:17 +0100

    feat: update deploy everything script

--- smoke-tests/deploy-everything.sh
+++ smoke-tests/deploy-everything.sh
@@ -13,9 +13,9 @@ export KUBECONFIG="${BASEDIR}/kubeconfig"
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.4/cert-manager.yaml
 
-kubectl apply -f certificates/selfsigned-cluster-issuer.yaml
-kubectl apply -f certificates/root-ca.yaml
-kubectl apply -f certificates/private-cluster-issuer.yaml
-kubectl apply -f certificates/peer-certificate.yaml
+kubectl apply -f certificates/
 
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.26/deploy/local-path-storage.yaml
+
+kubectl apply -f manifests/registry/
+kubectl apply -f manifests/dkv/
